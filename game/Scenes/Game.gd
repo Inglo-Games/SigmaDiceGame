99914348d8c7bbbfeb1125ca003c0eda44fe616f
Brett Haines <BHaines418@gmail.com>
2024-01-09 23:02:30 -0500

    Set viewport and camera for mobile displays

--- game/Scenes/Game.gd
+++ game/Scenes/Game.gd
@@ -8,14 +8,14 @@ const CAM_START_POS = Transform3D(
 		Vector3(1, 0, 0),
 		Vector3(0, 0.866025, -0.5),
 		Vector3(0, 0.5, 0.866025),
-		Vector3(0, 4, 2)
+		Vector3(0, 6.5, 7)
 )
 # Transform for camera during dice selection phase
 const CAM_END_POS = Transform3D(
 		Vector3(1, 0, 0),
-		Vector3(0, -0, -1),
-		Vector3(0, 1, -0),
-		Vector3(0, 2, -7)
+		Vector3(0, 0, -1),
+		Vector3(0, 1, 0),
+		Vector3(0, 6.5, -7)
 )
 
 # Game state and logic object
