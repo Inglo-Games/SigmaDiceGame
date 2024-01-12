767166cd6e2fa99881bcbc87ab76512a663ab547
Brett Haines <BHaines418@gmail.com>
2024-01-11 22:10:44 -0500

    Move scoreboard panel to bottom

--- game/Scenes/UI/ScoreboardPanel.gd
+++ game/Scenes/UI/ScoreboardPanel.gd
@@ -1,7 +1,8 @@
-extends HBoxContainer
+extends VBoxContainer
 
 # X coordinate for where panel should sit when "in view", relative to start pos
 const SLIDE_TARGET_X := 256.0
+const SLIDE_TARGET_Y := 512.0
 
 # Track whether panel is currently in the player's viewport
 var is_in_view := false 
@@ -17,32 +18,32 @@ func _on_expander_button_pressed():
 		_slide_panel_into_view()
 
 
-# Slide the panel left into the player's viewport
+# Slide the panel up into the player's viewport
 func _slide_panel_into_view():
 	# Determine new position for panel
-	var target_pos = Vector2(position.x - SLIDE_TARGET_X, position.y)
+	var target_pos = Vector2(position.x, position.y - SLIDE_TARGET_Y)
 	
 	# Use a tween to slide panel
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", target_pos, 1.5).set_trans(Tween.TRANS_SPRING)
+	tween.tween_property(self, "position", target_pos, 1.5).set_trans(Tween.TRANS_CUBIC)
 	tween.play()
 	await tween.finished
 	
 	# After tween is finished, update button and is_in_view value
 	is_in_view = true
-	$ExpanderButton.text = ">"
+	$ExpanderButton.text = "v"
 	$ExpanderButton.disabled = false
 
 
-# Slide the panel right out of the player's viewport
+# Slide the panel down out of the player's viewport
 func _slide_panel_out_view():
-	var target_pos = Vector2(position.x + SLIDE_TARGET_X, position.y)
+	var target_pos = Vector2(position.x, position.y + SLIDE_TARGET_Y)
 	
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", target_pos, 1.5).set_trans(Tween.TRANS_SPRING)
+	tween.tween_property(self, "position", target_pos, 1.5).set_trans(Tween.TRANS_CUBIC)
 	tween.play()
 	await tween.finished
 	
 	is_in_view = false
-	$ExpanderButton.text = "<"
+	$ExpanderButton.text = "^"
 	$ExpanderButton.disabled = false
