ae60594b1e5a3cc5dc9d05b3e9979e4b706b0a8c
Brett Haines <BHaines418@gmail.com>
2024-08-18 15:06:05 -0400

    Add flickering and optimize torch particles

--- game/Scenes/Envs/Torch.gd
+++ game/Scenes/Envs/Torch.gd
@@ -1,16 +1,18 @@
-extends OmniLight3D
+extends Node3D
 
 const LIGHT_ENERGY_OFFSET := 1.80
 const LIGHT_ENERGY_SCALE := 5.0
 
 @export_color_no_alpha var color := Color("c3781c")
 
+@onready var light : OmniLight3D = $OmniLight3D
+
 var noise_img : NoiseTexture2D
 var counter := 0.0
 
 func _ready() -> void:
 	# Set light color to export var
-	self.light_color = color
+	light.light_color = color
 	
 	# Generate grainy noise to simulate the flickering
 	noise_img = NoiseTexture2D.new()
@@ -18,9 +20,12 @@ func _ready() -> void:
 	noise_img.noise = FastNoiseLite.new()
 	noise_img.noise.seed = randi()
 	noise_img.noise.frequency = 0.55
+	
+	# Make point size of flame material larger
+	$GPUParticles3D.draw_pass_1.material.set_point_size(2.0)
 
 func _process(delta: float) -> void:
 	counter += delta
 	if(noise_img):
-		self.light_energy = LIGHT_ENERGY_OFFSET + \
+		light.light_energy = LIGHT_ENERGY_OFFSET + \
 				abs(noise_img.noise.get_noise_1d(counter)) * LIGHT_ENERGY_SCALE
